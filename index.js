c64b9bc43fc0a2580068fd6fa374345b17417363
SifatSararChistee <sifatsararchistee@gamil.com>
2025-02-20 11:41:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.esc8v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    const taskCollection = client.db("TaskManagerDB").collection("task");
+
+    //get all task for a user
+    app.get("/tasks/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //post task for a user
+    app.post("/tasks", async (req, res) => {
+      const taskData = req.body;
+      console.log("Received data:", taskData);
+      const result = await taskCollection.insertOne(taskData);
+      res.send(result);
+    });
+  } catch (error) {
+    console.error("Connection error:", error);
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World! task manager server running");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
