8eb71a25aec7bfabdf848ecaf5903bdad1d9e332
SifatSararChistee <sifatsararchistee@gamil.com>
2025-02-20 17:52:02 +0600

    task delete api

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ async function run() {
       res.send(result);
     });
 
+    //delete a task
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
     //post task for a user
     app.post("/tasks", async (req, res) => {
       const taskData = req.body;
